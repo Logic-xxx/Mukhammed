4b6d7c0e8ea336aa4f2097d29eb370d3372ea323
Logic-xxx <63334710+Logic-xxx@users.noreply.github.com>
2020-04-08 17:44:49 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+include('server.php')
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Registration page</title>
+</head>
+<body>
+<form>
+	<div class="reg">
+		<input type="text" name="Login" value="Login" placeholder="Username">
+		<input type="text" name="Email" value="Email" placeholder="Email">
+		<button class="btd" value="btn">Send</button>
+	</div>
+</form>
+</body>
+</html>
